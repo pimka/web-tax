5fa01bb2fe86ce63e9f1b958195c09c191db1acf
pimka <zelenova.yulia@mail.ru>
2017-06-25 12:53:23 +0500

    Add files via upload

--- mysite/static/js/game.js
+++ mysite/static/js/game.js
@@ -3,7 +3,6 @@ $(function() {
         event.preventDefault();
         number = $('#num').val();
         countChild = $('#otherValue').val();
-        result = Number(number.value)
         if (number.length > 0 && !isNaN(number) && number >= 1 && countChild >= 0) {
             console.log('click');
             $('#error').text('');
@@ -18,21 +17,32 @@ $(function() {
 
     function sendData() {
         console.log('sendData');
+        console.log('number', number);
+        console.log('countChild', countChild);
+        console.log('per13div', $('#radio13perdiv').prop('checked'));
+        console.log('per13', $('#radio13per').prop('checked'));
+        console.log('per30', $('#radio30per').prop('checked'));
+        console.log('checkChild', $('#checkradioCountChild').prop('checked'));
+        console.log('checkInv', $('#radioChildInv').prop('checked'));
         $.ajax({
             url : "",
             type : "POST",
             data : { number: $('#num').val(),
                      countChild: $('#otherValue').val(),
-                     per13div: $('radio13perdiv').val(),
-                     per13: $('radio13per').val(),
-                     per30: $('radio30per').val(),
-                     checkChild: $('checkradioCountChild').val(),
-                     checkInv: $('radioChildInv').val(),
+                     per13div: $('#radio13perdiv').is(':checked'),
+                     per13: $('#radio13per').is(':checked'),
+                     per30: $('#radio30per').is(':checked'),
+                     checkChild: $('#checkradioCountChild').is(':checked'),
+                     checkInv: $('#radioChildInv').is(':checked'),
                      csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value},
             success : function (json) {
                 $('#num').val('');
+                $('#otherValue').val('');
                 console.log(json);
                 $('#message').text(json['message'])
+                if (json['result'] !== 'none') {
+                    $('#button').prop('disabled', true);
+                }
                 console.log("success");
             },
             error : function(xhr, errmsg, err) {
